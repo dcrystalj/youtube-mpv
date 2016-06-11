5461195585308527b4c94740dcf6b5d9d27d9057
dcrystalj <dcrystalj@gmail.com>
2016-06-11 11:57:57 +0200

    upgrade

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,25 +1,31 @@
 #! /bin/sh
 
+PYTHON="/usr/bin/python3"
+YMPV_DIR="/opt/youtube-mpv"
+
 #install dependencies
 pip3 install youtube_dl
 
 #install server
-sudo mkdir /opt/youtube-mpv
-sudo cp ytdl_server.py /opt/youtube-mpv/
-sudo cp ytdl_config.py /opt/youtube-mpv/
+sudo mkdir $YMPV_DIR
+sudo cp ytdl_server.py $YMPV_DIR
+sudo cp ytdl_config.py $YMPV_DIR
 
 #install DAEMON
+sudo touch /etc/systemd/system/youtube-mpv.service
+sudo cat > "/etc/systemd/system/youtube-mpv.service" <<EOF
+[Unit]
+Description=Python server which can play youtube links
 
-#replace with your user
-sed -i -e 's/--chuid USERNAME/--chuid '`whoami`'/g' daemon
-sudo cp daemon /etc/init.d/youtube-mpv
-sudo chmod +x /etc/init.d/youtube-mpv
+[Service]
+Type=simple
+ExecStart=${PYTHON} "${YMPV_DIR}/ytdl_server.py"
 
-#run service
-#!!warning service should not be started as:
-#`service youtube-mpv start`
-#instead you should manually run as full path
-sudo /etc/init.d/youtube-mpv start
+[Install]
+WantedBy=default.target
+EOF
 
-#run daemon at startup
+
+sudo systemctl daemon-reload
+sudo systemctl restart youtube-mpv
 sudo systemctl enable youtube-mpv
